dca36fcefae8ffdc58df7724e2852d30e8e52267
shiawyonglim <65016718+shiawyonglim@users.noreply.github.com>
2022-03-03 16:26:05 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+particlesJS("particles", {
+  particles: {
+    number:{
+        value:90,
+        density:{
+            enable:true,
+        }
+    },
+    move:{
+        enable:true,
+        speed:2,
+    },
+    opacity:{
+        value:0.5,
+    },
+    color:{
+        value:'#fff',
+    },
+    size:{
+        value:9,
+    },
+  },
+});
+
